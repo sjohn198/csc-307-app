164e55b297057c63716d0c06ccdb3c183f33858e
sjohn198 <sjohn198@calpoly.edu>
2024-04-15 09:19:13 -0700

    implemented POST functionalty for /users

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -40,6 +40,11 @@ const findUserById = (id) =>  users["users_list"].find(
         (user) => user["id"] === id
     );
 
+const addUser = (user) => {
+    users["users_list"].push(user);
+    return user;
+};
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -64,6 +69,12 @@ app.get("/users/:id", (req, res) => {
         res.send(result);
     }
 });
+
+app.post("/users", (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send();
+})
 
 app.listen(port, () => {
     console.log(
